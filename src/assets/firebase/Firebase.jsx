aa32eea4d17ae03de4120102dd337c71d4af46a5
Juan Manuel Bolivar Mojica <jbolivarm2@ucentral.edu.co>
2026-03-06 11:40:13 -0500

    Config FireBase

---
+++ src/assets/firebase/Firebase.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDn0SYg3M8kAbU_UOjR6IIsHM7rbVGVIqA",
+  authDomain: "login-firebase-react-d2da1.firebaseapp.com",
+  projectId: "login-firebase-react-d2da1",
+  storageBucket: "login-firebase-react-d2da1.firebasestorage.app",
+  messagingSenderId: "446487512815",
+  appId: "1:446487512815:web:0fadd3fbbe438e7f803d1a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const getAuth = getAuth();
+
+export {app, getAuth}
+
